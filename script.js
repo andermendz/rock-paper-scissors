17e32121630b37251112eae0132c035d920fa038
andermendz <andersonmendz19@gmail.com>
2022-09-14 20:21:09 -0500

    computer choices done

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 console.log("import it's working")
 
-function getComputerChoice(){
+function getComputerChoice(max){
+
+    return Math.floor(Math.random() * max);
 
 }
 
+let choice = getComputerChoice(3);
+
+console.log(choice);
