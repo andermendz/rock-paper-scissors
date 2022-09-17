2c418cd6372a833765e735c9838fc4b20bd00e00
andermendz <andersonmendz19@gmail.com>
2022-09-16 19:54:05 -0500

    computer and player done

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 console.log("import it's working")
 
+
+
 function getComputerChoice(max){
 
-    return Math.floor(Math.random() * max);
+    let ComputerSelection =  Math.floor(Math.random() * 3);
+
+    // if (choice == 0){
+    //     choice = 'Rock'
+    // } else if (choice == 1){
+    //     choice = 'Paper'
+    // } else if (choice == 2){
+    //     choice = 'Scissors'
+    // }
+
+    return ComputerSelection;
 
 }
 
-let choice = getComputerChoice(3);
+function getPlayerSelection() {
 
-console.log(choice);
+    playerChoice = prompt("Select Rock, paper or Scissors")
+
+    let playerSelection = 0;
+    
+    if (playerChoice == 'rock' || 'ROCK' || 'Rock'){
+        playerSelection = 0;
+    } else if (playerChoice == 'paper' || 'PAPER' || 'Paper'){
+        playerSelection = 1;
+    } else if (playerChoice == 'scissors' || 'SCISSORS' || 'Scissors'){
+        playerSelection = 2;
+    } 
+
+    return playerSelection;
+}
+
+
+
+
+console.log(getComputerChoice());
+console.log(getPlayerSelection());
+
+
